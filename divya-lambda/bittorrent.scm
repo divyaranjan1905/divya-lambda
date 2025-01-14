8a4c78a93d361126c51de98a3277c90453b22fe5
Divya Ranjan <divya@subvertising.org>
2025-01-14 14:54:13 +0000

    Add rtorrent-xml-rpc

---
+++ divya-lambda/bittorrent.scm
@@ -0,0 +1,40 @@
+;;; GNU Guix --- Functional package management for GNU
+;;; Copyright © 2024 Divya R. Pattanaik <divya@subvertising.org>
+;;;
+;;; This file is not part of GNU Guix
+;;;
+;;; This program is free software: you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation, either version 3 of the License, or
+;;; (at your option) any later version.
+
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;;;
+;;; Packages related to torrenting
+(define-module (divya-lambda bittorrent)
+  #:use-module (guix packages)
+  #:use-module (guix download)
+  #:use-module (guix git-download)
+  #:use-module (guix build-system gnu)
+  #:use-module (guix gexp)
+  #:use-module (guix utils)
+  #:use-module (gnu packages bittorrent)
+  #:use-module (gnu packages xml))
+
+(define-public rtorrent-xml-rpc
+  (package
+    (inherit rtorrent)
+    (name "rtorrent-xml-rpc")
+    (arguments
+     `(#:configure-flags '("--with-xmlrpc-c")))
+    (inputs
+     (modify-inputs (package-inputs rtorrent)
+       (append xmlrpc-c)))))
+
+rtorrent-xml-rpc
